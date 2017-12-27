daef2c32a29ad90d94feab0af3fc2efce01f71af
hsperr <henning.sperr@gmail.com>
2017-12-27 11:35:25 +0900

    Initial commit

---
+++ run_four_player.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+./halite -d "240 160" "python3 MyBot.py" "python3 $1" "python3 $1" "python3 $1" | grep "rank #1\|seed"
+./halite -d "240 160" "python3 MyBot.py" "python3 $1" "python3 $1" "python3 $1" | grep "rank #1\|seed"
+./halite -d "240 160" "python3 MyBot.py" "python3 $1" "python3 $1" "python3 $1" | grep "rank #1\|seed"
+./halite -d "240 160" "python3 MyBot.py" "python3 $1" "python3 $1" "python3 $1" | grep "rank #1\|seed"
+./halite -d "240 160" "python3 MyBot.py" "python3 $1" "python3 $1" "python3 $1" | grep "rank #1\|seed"
